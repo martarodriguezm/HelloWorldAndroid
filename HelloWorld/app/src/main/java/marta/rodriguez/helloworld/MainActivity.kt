9e25b7c301ddbc227d13f7b74dc510d487aba999
Marta Rodriguez <rodriguez.martin.marta@gmail.com>
2024-08-21 13:33:32 +0200

    project created

---
+++ HelloWorld/app/src/main/java/marta/rodriguez/helloworld/MainActivity.kt
@@ -0,0 +1,26 @@
+package marta.rodriguez.helloworld
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import marta.rodriguez.helloworld.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left + 15, systemBars.top + 15, systemBars.right + 15, systemBars.bottom + 15)
+            insets
+        }
+
+    }
+}
